87be0867f0f1482fa3bd4acf3d3ee6017aa0a8ad
Jad-Bel <jadthegamer06@gmail.com>
2024-11-13 12:10:40 +0100

    Call 3 divs for form submission: submit button, success message, and error message

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,7 @@
 const submit = document.getElementById('submitButton');
 const successMessage = document.getElementById('successMessage');
 const errorMessage = document.getElementById('errorMessage');
+
+submit.addEventListener('click', async function(event) {
+    event.preventDefault();
+})
