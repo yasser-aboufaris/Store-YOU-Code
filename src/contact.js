ab2d7dd17e83d21758a3c6e4cbbb3d193d68dd61
Jad-Bel <jadthegamer06@gmail.com>
2024-11-13 16:34:08 +0100

    i've used ajax method to auto-fill the contact us form

--- src/contact.js
+++ src/contact.js
@@ -1,12 +1,32 @@
-const submit = document.getElementById('submitButton');
-const successMessage = document.getElementById('successMessage');
-const errorMessage = document.getElementById('errorMessage');
+// const submit = document.getElementById('submitButton');
+// const successMessage = document.getElementById('successMessage');
+// const errorMessage = document.getElementById('errorMessage');
 
-submit.addEventListener('click', async function(event) {
-    event.preventDefault();
+// submit.addEventListener('click', async function(event) {
+//     event.preventDefault();
 
-    const nameInput = document.getElementById('userInput');
-    const lastNameInput = document.getElementById('lastInput');
-    const subjectInput = document.getElementById('subjectInput');
-    const textInput = document.getElementById('textInput');
-})
+//     const nameInput = document.getElementById('userInput');
+//     const lastNameInput = document.getElementById('lastInput');
+//     const subjectInput = document.getElementById('subjectInput');
+//     const textInput = document.getElementById('textInput');
+// })
+
+document.getElementById('fillForm').addEventListener('click', function () {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'https://randomuser.me/api/', true);
+
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState ==  4)  {
+            if(xhr.status == 200) {
+              //console.log(JSON.parse(xhr.responseText))
+                parseResponse(xhr.responseText);
+            } else {
+                console.error('Error!');
+            }
+        }
+
+    }
+
+    xhr.send();
+});
