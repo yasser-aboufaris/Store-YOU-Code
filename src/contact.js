cadb985153866d052aced7abf7b52d0e1de9847f
Jad-Bel <jadthegamer06@gmail.com>
2024-11-13 12:05:03 +0100

    Call 3 divs for form submission: submit button, success message, and error message

---
+++ src/contact.js
@@ -0,0 +1,3 @@
+const submit = document.getElementById('submitButton');
+const successMessage = document.getElementById('successMessage');
+const errorMessage = document.getElementById('errorMessage');
