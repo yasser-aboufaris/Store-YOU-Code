2d107a3a4ff8a364edafbab94d2ae9d10200794e
Jad-Bel <jadthegamer06@gmail.com>
2024-11-14 10:12:02 +0100

    Added functionality to update user data on the server using PUT request

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,6 @@
-// const submit = document.getElementById('submitButton');
-// const successMessage = document.getElementById('successMessage');
-// const errorMessage = document.getElementById('errorMessage');
+const submit = document.getElementById('submitButton');
+const successMessage = document.getElementById('successMessage');
+const errorMessage = document.getElementById('errorMessage');
 
 // submit.addEventListener('click', async function(event) {
 //     event.preventDefault();
@@ -19,7 +19,7 @@ document.getElementById('fillForm').addEventListener('click', function () {
     xhr.onreadystatechange = function () {
         if(xhr.readyState ==  4)  {
             if(xhr.status == 200) {
-              //console.log(JSON.parse(xhr.responseText))
+            //   console.log(JSON.parse(xhr.responseText))
                 parseResponse(xhr.responseText);
             } else {
                 console.error('Error!');
@@ -34,7 +34,7 @@ document.getElementById('fillForm').addEventListener('click', function () {
 
 function parseResponse(response) {
     let data = JSON.parse(response);
-    // console.log(typeof(data))
+    console.log(typeof(data))
     let results = data.results[0];
     
     document.getElementById("userInput").value = `${results.name.first}`;
@@ -42,3 +42,17 @@ function parseResponse(response) {
     document.getElementById("subjectInput").value = `${results.login.username}`;
     document.getElementById("textInput").value = `${results.login.username} - ${results.name.title} ${results.name.first} ${results.name.last} - Age: ${results.dob.age} - Registered for: ${results.registered.age} years`;
 }
+
+document.getElementById('submitButton').addEventListener('click', function () {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('PUT', 'https://67353a845995834c8a923af7.mockapi.io/contactForm/put', true);
+
+    xhr.setRequestHeader('content-Type', 'application/json');
+
+    const data = {
+        firstName: document.getElementById('userInput').value,
+        lastName: document.getElementById('lastInput').value,
+        subject: document.getElementById('subjectInput').value,
+        text: document.getElementById('textInput').value
+    }
