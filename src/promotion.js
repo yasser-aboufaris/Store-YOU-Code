a2d3bbed9e6fa5abeffb0af82e8f10bb60ed9b77
Jad-Bel <jadthegamer06@gmail.com>
2024-11-16 20:13:18 +0100

    Display limited discounted products

--- src/promotion.js
+++ src/promotion.js
@@ -1,2 +1,20 @@
 let startIndex = 0;
 const itemsPerPage = 4;
+
+document.addEventListener('DOMContentLoaded', function () {
+    axios.get('../products.json')
+    .then(function (response) {
+        const products = response.data;
+
+        const discountedProducts = products.filter(function (products) {
+            return products.discount !== null;
+        });
+
+        console.log(discountedProducts);
+        displayProducts(discountedProducts.slice(0, itemsPerPage), '#products-container');
+        displayProducts(discountedProducts.slice(itemsPerPage, itemsPerPage * 2), '#products-container-2');
+    });
+})
+.catch(function (error) {
+    console.error('Error fetching:', error);
+});
